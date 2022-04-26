105b4613c5a4abb243645752239b2242c49a197d
publicacionweb22 <publicacionweb22@gmail.com>
2022-04-26 11:57:16 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Métodos y propiedades de acceso al DOM con JavaScript
+
+// El DOM es un árbol que usa el navegador para, en cada nodo de ese arbol, asociar un elemento HTML con
+// un objeto JavaScript con propiedades y métodos para poder modificar ese elemento de forma
+// programática
+
+var title = document.getElementById('title');
+
+title.innerHTML = 'Otro título';
